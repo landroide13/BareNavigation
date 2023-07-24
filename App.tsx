f2a9c8c8a42cf20f14022d7e38ad5f3ffec96d74
Luis Andrade <landroide@Luiss-MacBook-Pro.local>
2023-07-25 10:55:56 +1200

    start Nav

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App Navigation</Text>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
